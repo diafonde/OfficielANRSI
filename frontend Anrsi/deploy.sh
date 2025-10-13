ed70dd058caccc7f6466ff079ff42fb98ae6b8c3
diafonde <diamoussa@gmail.com>
2025-10-13 13:22:00 +0000

    anrsi version 3

--- frontend Anrsi/deploy.sh
+++ frontend Anrsi/deploy.sh
@@ -2,7 +2,7 @@
 git add .
 git commit -m "anrsi version 3"
 git push
-ng build --configuration production --base-href "/ANRSIV3/" --output-path docs
+ng build --configuration production --base-href "/OfficielANRSI/" --output-path docs
 cp docs/index.html docs/404.html
 git add docs/
 git commit -m "Déploiement automatique"
