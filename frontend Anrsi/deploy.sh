7bb42eda9a8e883e06798d613a3220e03ac5f1e3
diafonde <diamoussa@gmail.com>
2025-10-13 13:32:38 +0000

    anrsi version 3 - fixed budget warnings

--- frontend Anrsi/deploy.sh
+++ frontend Anrsi/deploy.sh
@@ -4,6 +4,8 @@ git commit -m "anrsi version 3 - fixed budget warnings"
 git push
 ng build --configuration production --base-href "/OfficielANRSI/" --output-path docs
 cp docs/index.html docs/404.html
+touch docs/.nojekyll
+echo "anrsi.mr" > docs/CNAME
 git add docs/
 git commit -m "Déploiement automatique - budget warnings resolved"
 git push origin main
